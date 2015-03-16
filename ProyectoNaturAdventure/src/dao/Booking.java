c1b1a960d3fa8bb1ab6c01000e71625a605c2a49
Xavi-nho <xavigarciam6@hotmail.com>
2015-03-16 17:52:01 +0100

    Booking

--- ProyectoNaturAdventure/src/dao/Booking.java
+++ ProyectoNaturAdventure/src/dao/Booking.java
@@ -1,5 +1,79 @@
 package dao;
 
-public class Booking {
+import common.Date;
 
+public class Booking {
+	private int codBooking;
+	private Date proposalPerformingDate;
+	private int numPartakers;
+	private Date bookingDate;
+	private String customerNif;
+	private StartHour starHour;
+	private String status;
+	
+	public Booking(Date proposalPerformingDate, int numPartakers,
+			Date bookingDate, String customerNif, StartHour starHour) {
+		this.proposalPerformingDate = proposalPerformingDate;
+		this.numPartakers = numPartakers;
+		this.bookingDate = bookingDate;
+		this.customerNif = customerNif;
+		this.starHour = starHour;
+	}
+
+	public int getCodBooking() {
+		return codBooking;
+	}
+
+	public void setCodBooking(int codBooking) {
+		this.codBooking = codBooking;
+	}
+
+	public Date getProposalPerformingDate() {
+		return proposalPerformingDate;
+	}
+
+	public void setProposalPerformingDate(Date proposalPerformingDate) {
+		this.proposalPerformingDate = proposalPerformingDate;
+	}
+
+	public int getNumPartakers() {
+		return numPartakers;
+	}
+
+	public void setNumPartakers(int numPartakers) {
+		this.numPartakers = numPartakers;
+	}
+
+	public Date getBookingDate() {
+		return bookingDate;
+	}
+
+	public void setBookingDate(Date bookingDate) {
+		this.bookingDate = bookingDate;
+	}
+
+	public String getCustomerNif() {
+		return customerNif;
+	}
+
+	public void setCustomerNif(String customerNif) {
+		this.customerNif = customerNif;
+	}
+
+	public StartHour getStarHour() {
+		return starHour;
+	}
+
+	public void setStarHour(StartHour starHour) {
+		this.starHour = starHour;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
 }
